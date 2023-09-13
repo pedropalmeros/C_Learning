5e960d7f5d7c7186f1d80e18a74b29f5ae8eab31
Palmeros <pedrofpalmeros@gmail.com>
2023-09-13 17:06:55 -0600

    Adding base files and structures

---
+++ Unit_03_Vars/program_01/program_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ this probram illustrate the using of multiline and single line comments
+ */
+
+int main(void){
+    // It prints - THIS PROGRAM CONTAINS COMMENTS
+    printf("THIS PROGRAM CONTAINS COMMENTS\n");
+    return 0;
+}
